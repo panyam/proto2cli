5c2f6e772fb9735e9291d66148be5b5d0bc43e37
Sri Panyam <sri.panyam@gmail.com>
2023-03-22 18:50:32 -0700

    Adding a label proto too

--- examples/musicservice/gen/models.pb.go
+++ examples/musicservice/gen/models.pb.go
@@ -315,6 +315,92 @@ func (x *Album) GetReleaseDate() *timestamppb.Timestamp {
 	return nil
 }
 
+// A record label company.   This entity is the owner of albums as they produce and release them.
+// Note unlike the Album entity the Label does not contain a (repeated) field of all albums it
+// owns.   This is because the way the albums are added to or removed from a Label's collection
+// is via the API.
+type Label struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	CreatedAt *timestamppb.Timestamp `protobuf:"bytes,1,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
+	UpdatedAt *timestamppb.Timestamp `protobuf:"bytes,2,opt,name=updated_at,json=updatedAt,proto3" json:"updated_at,omitempty"`
+	// ID of this label
+	Id string `protobuf:"bytes,3,opt,name=id,proto3" json:"id,omitempty"`
+	// Name of this record label company (eg, "Virgin Records")
+	Name string `protobuf:"bytes,4,opt,name=name,proto3" json:"name,omitempty"`
+	// When this company was established
+	EstablishedDate *timestamppb.Timestamp `protobuf:"bytes,5,opt,name=established_date,json=establishedDate,proto3" json:"established_date,omitempty"`
+}
+
+func (x *Label) Reset() {
+	*x = Label{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_models_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Label) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Label) ProtoMessage() {}
+
+func (x *Label) ProtoReflect() protoreflect.Message {
+	mi := &file_models_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Label.ProtoReflect.Descriptor instead.
+func (*Label) Descriptor() ([]byte, []int) {
+	return file_models_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *Label) GetCreatedAt() *timestamppb.Timestamp {
+	if x != nil {
+		return x.CreatedAt
+	}
+	return nil
+}
+
+func (x *Label) GetUpdatedAt() *timestamppb.Timestamp {
+	if x != nil {
+		return x.UpdatedAt
+	}
+	return nil
+}
+
+func (x *Label) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *Label) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *Label) GetEstablishedDate() *timestamppb.Timestamp {
+	if x != nil {
+		return x.EstablishedDate
+	}
+	return nil
+}
+
 var File_models_proto protoreflect.FileDescriptor
 
 var file_models_proto_rawDesc = []byte{
@@ -371,10 +457,24 @@ var file_models_proto_rawDesc = []byte{
 	0x0c, 0x72, 0x65, 0x6c, 0x65, 0x61, 0x73, 0x65, 0x5f, 0x64, 0x61, 0x74, 0x65, 0x18, 0x08, 0x20,
 	0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f,
 	0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x52,
-	0x0b, 0x72, 0x65, 0x6c, 0x65, 0x61, 0x73, 0x65, 0x44, 0x61, 0x74, 0x65, 0x42, 0x1f, 0x5a, 0x1d,
-	0x6d, 0x75, 0x73, 0x69, 0x63, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6d, 0x75, 0x73, 0x69, 0x63, 0x73,
-	0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x62, 0x06, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x0b, 0x72, 0x65, 0x6c, 0x65, 0x61, 0x73, 0x65, 0x44, 0x61, 0x74, 0x65, 0x22, 0xe8, 0x01, 0x0a,
+	0x05, 0x4c, 0x61, 0x62, 0x65, 0x6c, 0x12, 0x39, 0x0a, 0x0a, 0x63, 0x72, 0x65, 0x61, 0x74, 0x65,
+	0x64, 0x5f, 0x61, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x67, 0x6f, 0x6f,
+	0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x54, 0x69, 0x6d,
+	0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x52, 0x09, 0x63, 0x72, 0x65, 0x61, 0x74, 0x65, 0x64, 0x41,
+	0x74, 0x12, 0x39, 0x0a, 0x0a, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x5f, 0x61, 0x74, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d,
+	0x70, 0x52, 0x09, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x41, 0x74, 0x12, 0x0e, 0x0a, 0x02,
+	0x69, 0x64, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x12, 0x12, 0x0a, 0x04,
+	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65,
+	0x12, 0x45, 0x0a, 0x10, 0x65, 0x73, 0x74, 0x61, 0x62, 0x6c, 0x69, 0x73, 0x68, 0x65, 0x64, 0x5f,
+	0x64, 0x61, 0x74, 0x65, 0x18, 0x05, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x67, 0x6f, 0x6f,
+	0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x54, 0x69, 0x6d,
+	0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x52, 0x0f, 0x65, 0x73, 0x74, 0x61, 0x62, 0x6c, 0x69, 0x73,
+	0x68, 0x65, 0x64, 0x44, 0x61, 0x74, 0x65, 0x42, 0x1f, 0x5a, 0x1d, 0x6d, 0x75, 0x73, 0x69, 0x63,
+	0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6d, 0x75, 0x73, 0x69, 0x63, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63,
+	0x65, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -389,27 +489,31 @@ func file_models_proto_rawDescGZIP() []byte {
 	return file_models_proto_rawDescData
 }
 
-var file_models_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_models_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_models_proto_goTypes = []interface{}{
 	(*Song)(nil),                  // 0: musicservice.Song
 	(*Artist)(nil),                // 1: musicservice.Artist
 	(*Album)(nil),                 // 2: musicservice.Album
-	(*timestamppb.Timestamp)(nil), // 3: google.protobuf.Timestamp
+	(*Label)(nil),                 // 3: musicservice.Label
+	(*timestamppb.Timestamp)(nil), // 4: google.protobuf.Timestamp
 }
 var file_models_proto_depIdxs = []int32{
-	3, // 0: musicservice.Song.created_at:type_name -> google.protobuf.Timestamp
-	3, // 1: musicservice.Song.updated_at:type_name -> google.protobuf.Timestamp
-	3, // 2: musicservice.Artist.created_at:type_name -> google.protobuf.Timestamp
-	3, // 3: musicservice.Artist.updated_at:type_name -> google.protobuf.Timestamp
-	3, // 4: musicservice.Artist.date_of_birth:type_name -> google.protobuf.Timestamp
-	3, // 5: musicservice.Album.created_at:type_name -> google.protobuf.Timestamp
-	3, // 6: musicservice.Album.updated_at:type_name -> google.protobuf.Timestamp
-	3, // 7: musicservice.Album.release_date:type_name -> google.protobuf.Timestamp
-	8, // [8:8] is the sub-list for method output_type
-	8, // [8:8] is the sub-list for method input_type
-	8, // [8:8] is the sub-list for extension type_name
-	8, // [8:8] is the sub-list for extension extendee
-	0, // [0:8] is the sub-list for field type_name
+	4,  // 0: musicservice.Song.created_at:type_name -> google.protobuf.Timestamp
+	4,  // 1: musicservice.Song.updated_at:type_name -> google.protobuf.Timestamp
+	4,  // 2: musicservice.Artist.created_at:type_name -> google.protobuf.Timestamp
+	4,  // 3: musicservice.Artist.updated_at:type_name -> google.protobuf.Timestamp
+	4,  // 4: musicservice.Artist.date_of_birth:type_name -> google.protobuf.Timestamp
+	4,  // 5: musicservice.Album.created_at:type_name -> google.protobuf.Timestamp
+	4,  // 6: musicservice.Album.updated_at:type_name -> google.protobuf.Timestamp
+	4,  // 7: musicservice.Album.release_date:type_name -> google.protobuf.Timestamp
+	4,  // 8: musicservice.Label.created_at:type_name -> google.protobuf.Timestamp
+	4,  // 9: musicservice.Label.updated_at:type_name -> google.protobuf.Timestamp
+	4,  // 10: musicservice.Label.established_date:type_name -> google.protobuf.Timestamp
+	11, // [11:11] is the sub-list for method output_type
+	11, // [11:11] is the sub-list for method input_type
+	11, // [11:11] is the sub-list for extension type_name
+	11, // [11:11] is the sub-list for extension extendee
+	0,  // [0:11] is the sub-list for field type_name
 }
 
 func init() { file_models_proto_init() }
@@ -454,6 +558,18 @@ func file_models_proto_init() {
 				return nil
 			}
 		}
+		file_models_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Label); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -461,7 +577,7 @@ func file_models_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_models_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
